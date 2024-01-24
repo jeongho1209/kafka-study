346243824a3b055f4ded1a71bab927e464633b8b
이정호 <dlwjdgh1115@gmail.com>
2024-01-24 11:45:38 +0900

    add :: KafkaTopicConstants

---
+++ src/main/kotlin/com/example/kafkastudy/test2_slack/KafkaTopicConstants.kt
@@ -0,0 +1,6 @@
+package com.example.kafkastudy.test2_slack
+
+object KafkaTopicConstants {
+    const val TOPIC_SLACK_REPORT = "SlackReport"
+    const val TOPIC_FAIL_SLACK_REPORT = "FailSlackReport"
+}
